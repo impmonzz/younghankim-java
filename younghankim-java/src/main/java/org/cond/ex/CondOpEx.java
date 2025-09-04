f04a332613a1d8ccdb354eee8209da46cc157fd2
impmonzz <khss8070@gmail.com>
2025-09-04 16:24:00 +0900

    ✨ feat: 조건문 실습 코드 추가

---
+++ younghankim-java/src/main/java/org/cond/ex/CondOpEx.java
@@ -0,0 +1,10 @@
+package org.cond.ex;
+
+public class CondOpEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max = (a>b) ? a : b;
+        System.out.println("더큰 숫자는 " + max +  "입니다. ");
+    }
+}
